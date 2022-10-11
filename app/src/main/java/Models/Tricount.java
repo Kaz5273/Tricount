3c6378c277b75b88de392f612f5f8f46ca8a7535
allard.kazuki <kazutor73@gmail.com>
2022-10-11 13:35:26 +0200

    first commit

---
+++ app/src/main/java/Models/Tricount.java
@@ -0,0 +1,15 @@
+package Models;
+
+import java.util.Date;
+
+public class Tricount {
+    private String titre;
+    private String description;
+    private Date date;
+
+public Tricount(String titre, String description, Date date){
+    this.titre = titre;
+    this.description = description;
+    this.date = date;
+}
+}
